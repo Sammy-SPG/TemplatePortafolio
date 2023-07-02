4564a67a19d17483f4b3fae890972a25899b669f
Sammy <samuel@blackrobot.mx>
2023-07-02 12:56:03 -0600

    Certificacion del ingeniero

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     menu();
 });
 
-    let ubicacionPrincipal = window.pageYOffset;
-const menu = ()=>{
+let ubicacionPrincipal = window.pageYOffset;
+const menu = () => {
     let desplazamientoActual = window.pageYOffset;
-    if(ubicacionPrincipal >= desplazamientoActual && screen.width > 800){
+    if (ubicacionPrincipal >= desplazamientoActual && screen.width > 800) {
         document.getElementById("nav").style.top = '0px';
     }
-    else if(ubicacionPrincipal != desplazamientoActual && screen.width <= 800){
+    else if (ubicacionPrincipal != desplazamientoActual && screen.width <= 800) {
         document.getElementById("nav").style.top = '0px';
-    }else{
+    } else {
         document.getElementById("nav").style.top = '-100px';
     }
 
@@ -18,20 +18,34 @@ const menu = ()=>{
 }
 
 
- const hamburguesa = document.querySelector(".hamburguesa");
+const hamburguesa = document.querySelector(".hamburguesa");
 
- const menuDesplazable = document.querySelector('.menu-navegacion');
+const menuDesplazable = document.querySelector('.menu-navegacion');
 
- hamburguesa.addEventListener("click",()=>{
+hamburguesa.addEventListener("click", () => {
     let boleano = menuDesplazable.classList.toggle("spread");
- });
+});
 
- window.addEventListener('click',e=>{
-     if(menuDesplazable.classList.contains('spread') && e.target != menuDesplazable
-            && e.target != hamburguesa){
-                menuDesplazable.classList.toggle('spread');
-     }
- });
+window.addEventListener('click', e => {
+    if (menuDesplazable.classList.contains('spread') && e.target != menuDesplazable
+        && e.target != hamburguesa) {
+        menuDesplazable.classList.toggle('spread');
+    }
+});
+
+document.getElementById('infoToggle').addEventListener('click', function () {
+
+    if (!document.querySelector('.container-cer').classList.contains('show')) {
+        let template_certificacion = document.getElementById('template-certificacion');
+        let clone_cer = template_certificacion.content.cloneNode(true);
+        document.querySelector('.container-cer').appendChild(clone_cer);
+        document.querySelector('.chevron').setAttribute('src', 'https://img.icons8.com/pastel-glyph/54/circled-chevron-up.png');
+    } else {
+        document.querySelector('.container-cer').innerHTML = '';
+        document.querySelector('.chevron').setAttribute('src', 'https://img.icons8.com/ios/54/circled-chevron-down.png');
+    }
+
+    document.querySelector('.container-cer').classList.toggle('show');
+});
 
 
- 
